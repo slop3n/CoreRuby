d5b1ffd86b69451a5868bf1d0baa3ef32b47f78a
Lyubomir Dimitrov <slop3n@gmail.com>
2014-10-21 19:44:23 +0300

    added solution for week2, hashes

--- week1/1-fmi-tribute/solution.rb
+++ week1/1-fmi-tribute/solution.rb
@@ -1,3 +1,26 @@
 class Array
-  # Your code goes here.
+  def to_hash
+    each_with_object({}) { |element, hash| hash[element[0]] = element[1] }
+  end
+
+  def index_by(&funct)
+
+  end
+
+  def subarray_count(subarray)
+    count = 0
+    self.each_cons(subarray.size) do |x|
+      count += 1 if x == subarray
+    end
+    count
+  end
+
+  def occurences_count
+  self.to_s.downcase
+  times_met = Hash.new(0)
+  0.upto(self.length - 1) do |x|
+    times_met[self[x]] += 1
+  end
+  times_met
+  end
 end
--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,32 @@
 class Hash
-  # Your code goes here.
+  def pick(*keys)
+    self.select { |key,value| keys.include?(key) }
+  end
+
+  def except(*keys)
+    hash = self
+    keys.each do |x|
+    hash.delete_if { |key, value| x == key }
+    end
+    hash
+  end
+
+  def compact_values
+    self.select { |key, value| value && true }
+  end
+
+  def defaults(hash)
+    
+  end
+
+  def except!(*keys)
+      keys.each do |x|
+      self.delete_if { |key, value| x == key }
+      end
+      self
+  end
 end
+
+puts({"a" => 5, "b" => 6, "2" => 3, "c" => nil, "d" => false}.pick("b", "2"))
+puts({"a" => 5, "b" => 6, "2" => 3, "c" => nil, "d" => false}.except("b", "2"))
+puts({"a" => 5, "b" => 6, "2" => 3, "c" => nil, "d" => false}.compact_values)
